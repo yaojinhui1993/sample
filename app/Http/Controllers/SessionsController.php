ebc6cbbfac92d81846d3a950b1ba418c36b5771a
Jinhui Yao <yaojinhui1993@gmail.com>
2018-08-22 06:25:35 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -15,14 +15,16 @@ class SessionsController extends Controller
     {
         $credentials = $this->validate(request(), [
            'email' => 'required|email|max:255',
-           'password' => 'required'
+           'password' => 'required',
         ]);
 
         if (Auth::attempt($credentials, request()->has('remember'))) {
             session()->flash('success', '欢迎回来！');
+
             return redirect()->route('users.show', [Auth::user()]);
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+
             return redirect()->back();
         }
 
@@ -34,6 +36,7 @@ class SessionsController extends Controller
         Auth::logout();
 
         session()->flash('success', '您已成功退出！');
+
         return redirect('login');
     }
 }
