7b6ed2ed13e3bb76c68fc006b5b80302a3be0928
yaojinhui <yaojinhui1993@gmail.com>
2018-08-22 17:13:13 +0800

    show status

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Carbon::setLocale('zh');
     }
 
     /**
